e37602d0613dce2d43f072250550ae0c011ff428
AameenaSana <aameenasana180705@gmail.com>
2026-06-15 12:21:44 +0000

    Fixed Selenium headless mode

--- src/test/java/com/selenium/AppTest.java
+++ src/test/java/com/selenium/AppTest.java
@@ -12,9 +12,10 @@ public class AppTest {
 
         ChromeOptions options = new ChromeOptions();
 
-        
+        options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
 
